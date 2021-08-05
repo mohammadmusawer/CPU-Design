742fead042c5f81f5b8eb3495534d522aa882a8f
Mohammad Musawer <66740014+mohammadmusawer@users.noreply.github.com>
2021-08-05 15:03:46 -0400

    Initial commit

---
+++ CPU_Design.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Mohammad Musawer
+// 
+// Create Date: 08/05/2021 02:40:10 PM
+// Project Name: Central Processing Unit Design in Verilog
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module control_unit(
+
+    );
+endmodule
+
+
+module ALU(
+
+    );
+endmodule
+
+
+module RAM(
+
+    );
+endmodule
+
+
+module register(
+
+    );
+endmodule
